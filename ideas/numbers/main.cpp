9cf564b32553babbb4fbae83a94fad88621cc606
hamad <mo7maaa7maaa@gmail.com>
2025-04-24 16:51:27 +0200

    second problem

---
+++ ideas/numbers/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a ;
+    cin >> n ;
+    int even = 0 , odd= 0 , pos = 0, neg = 0 ;
+    for (int i = 0; i < n ; i++){
+        cin >> a ; 
+            if (a % 2 == 0 ){
+            even++;
+        }else 
+        {
+            odd++;
+        }
+        if(a > 0 ){
+            pos++;
+        }else{
+            neg++;
+        }
+    }
+    cout<<"Even: "<<even<<endl;
+    cout<<"Odd: "<<odd<<endl;
+    cout<<"Positive: "<<pos<<endl;
+    cout<<"Negative: "<<neg<<endl;
+
+
+    return 0;
+}
